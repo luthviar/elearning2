e4c3bf5b636fae3943c754dffab5692ef1e1b923
rohmattaufik <rohmat.taufik14@gmail.com>
2018-01-03 09:16:44 +0700

    update material file

--- resources/views/material.blade.php
+++ resources/views/material.blade.php
@@ -9,7 +9,7 @@
       <p>{{ $chapter['material']->description    }}</p>
       <h4>Attachments :</h4>
       @foreach ($chapter['material']['files_material'] as $file)
-      <div style="padding-bottom: 10px;"><a href="/test" class="btn btn-default" style="width: 100%;">{{ $file->name}}</a></div>
+      <div style="padding-bottom: 10px;"><a href="/file" class="btn btn-default" style="width: 100%;">{{ $file->name}}</a></div>
       @endforeach
       <a href="{{ url('/finish_chapter', $chapter->id) }}" class="btn btn-success">Finish this chapter</a>
     </div>
