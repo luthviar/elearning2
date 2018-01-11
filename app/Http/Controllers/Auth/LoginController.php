93e33650a73c38a4819890b79135a7b5d35e743b
rohmattaufik <rohmat.taufik14@gmail.com>
2018-01-11 20:52:11 +0700

    user flag active

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function credentials(\Illuminate\Http\Request $request)
+    {
+        //return $request->only($this->username(), 'password');
+        return ['email' => $request->email, 'password' => $request->password, 'flag_active' => 1];
+    }
 }
